10e7261cc6d65b1797ef7bdd749d6983bc11cffa
Mintesnot <mintesnotfikre8@gmail.com>
2025-03-16 00:26:33 -0700

    update user model

--- src/models/User.js
+++ src/models/User.js
@@ -4,10 +4,11 @@ const userSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     chatId: { type: String, required: true, unique: true },
     username: { type: String, unique: true },
-    name: { type: String },
-    phone: { type: Number },
+    firstName: { type: String, required: true },
+    lastName: { type: String},
+    phoneNumber: { type: Number },
     balance: { type: Number, default: 0.0 },
-    isAdmin: { type: Boolean, default: false }, // Admin flag
+    isBan: { type: Boolean, default: false }, // Admin flag
 }, { timestamps: true });
 
 const User = mongoose.model('betUser', userSchema);
